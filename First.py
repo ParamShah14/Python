84426fd8bd462768fff540368ae0922dd2fc9a4f
Param <154975225+ParamShah14@users.noreply.github.com>
2026-03-29 18:03:04 +0530

    Add files via upload

---
+++ First.py
@@ -0,0 +1,10 @@
+# first_program.py
+
+"""
+This is my first Python program.
+It prints a welcome message and demonstrates basic syntax.
+"""
+
+# Printing a simple message
+print("Hello, World!")
+
